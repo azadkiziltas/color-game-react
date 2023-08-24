0110b7057740ff6f02a428b8ce911e7ebf13b61b
Azad Kızıltaş <diyarbis@gmail.com>
2023-08-25 01:02:25 +0300

    update col

--- src/GameContainer.jsx
+++ src/GameContainer.jsx
@@ -62,7 +62,7 @@ export default function GameContainer() {
         <>
             <div className="">
                 <div className="row justify-content-center">
-                    <div className="col-md-9 p-5">
+                    <div className="col-md-7 p-5">
                         <div className="card">
                             <h1
                                 id="selectedColor"
